22eb2a99e639af6c84957767fe996a585d34504b
Shibu Dhara <shibu.bluehorse@gmail.com>
2026-01-29 21:20:20 +0530

    build issue resolved

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,5 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
@@ -10,33 +8,19 @@ import { SocialSidebar } from "@/components/SocialSidebar";
 import { ScrollToTopButton } from "@/components/ScrollToTopButton";
 import TanstackProvider from "@/components/TanstackProvider";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
 export const metadata: Metadata = {
   title: "Sundarban Bengal Trip",
   description: "Sundarban Bengal Trip",
 };
 
-const queryClient = new QueryClient();
-
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className="antialiased">
         <TanstackProvider>
           <TooltipProvider>
             <Toaster />
