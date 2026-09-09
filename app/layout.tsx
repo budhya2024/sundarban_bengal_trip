64dc2e2413d4aa39342de7116dc68376061f99ef
budhyadas <budhya.bluehorse@gmail.com>
2026-09-09 18:20:37 +0530

    feat: add Google Tag Manager (GTM-W7JXTT3M)

--- app/layout.tsx
+++ app/layout.tsx
@@ -252,6 +252,29 @@ export default function RootLayout({
         />
       </head>
       <body className={`${ibmPlexSans.variable} antialiased`}>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-W7JXTT3M"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+
+        {/* Google Tag Manager */}
+        <Script
+          id="google-tag-manager"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-W7JXTT3M');`,
+          }}
+        />
+
         {/* Local Business + Travel Agency Schema */}
         <Script
           id="travel-agency-schema"
